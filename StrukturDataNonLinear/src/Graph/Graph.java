f6a1ea1cc6f5df2c1410f143dd62c5f1e8a1418e
Aureli Isaias <Aureli Isaias@DESKTOP-TD137MK>
2019-02-27 14:16:28 +0700

    Membuat Graph

--- StrukturDataNonLinear/src/Graph/Graph.java
+++ StrukturDataNonLinear/src/Graph/Graph.java
@@ -17,57 +17,6 @@ public class Graph {
     int [][] adjacencyMatrik;
     int countVertex = 0;
 
-    public Graph() {
-        vertexList = new char[maxVertex];
-        adjacencyMatrik = new int [maxVertex] [maxVertex];
-        countVertex = 0;
-        for (int i = 0; i < vertexList.length; i++) {
-            for (int j = 0; j < vertexList.length; j++) {
-                if (i == j) {
-                    adjacencyMatrik[i][j] = 0;
-                }
-                else {
-                    adjacencyMatrik[i][j] = -1;
-                }
-            }
-        }
-    }
-    
-    public void addVertex(char node){
-        vertexList[countVertex++] = node; 
-    }
-    
-    public void addEdge(int x, int y, int z){
-        adjacencyMatrik[x][y] = z;
-        adjacencyMatrik[y][x] = z;
         
-    }
-    
-    public void addEdge(char x, char y, int z){
-        adjacencyMatrik[indexVertex(x)][indexVertex(y)] = z;
-        adjacencyMatrik[indexVertex(y)][indexVertex(x)] = z;
-    }
-    
-    public int indexVertex(char index){
-        for (int i = 0; i < countVertex; i++) {
-            if (vertexList[i] == index) {
-                return i;
-            }
-        }
-        return -1;
-    }
-    
-    public void printaddjecency(){
-        for (int baris = 0; baris < countVertex; baris++) {
-            for (int kolom = 0; kolom < countVertex; kolom++) {
-                System.out.printf(adjacencyMatrik[baris][kolom]+" ");
-            }
-            System.out.println("");
-        }
-    }
-    
-    public int getWeight(char x, char y){
-        return adjacencyMatrik[indexVertex(x)][indexVertex(y)];
-    }    
     
 }
